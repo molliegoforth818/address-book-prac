73552ef09ebdee4651050f37fea77431e253cf13
Mollie Goforth <molliegoforth818@gmail.com>
2020-01-23 15:14:27 -0600

    completed practice

--- scripts/address.js
+++ scripts/address.js
@@ -1,20 +1,25 @@
-// Some starter code
-const container = document.querySelector("#wishList")
+const container = document.getElementById("container")
+// container is where i want the below actions to happen id needs to match in html //
 
-document.querySelector("#saveWishList").addEventListener("click", event => {
+document.getElementById("saveWishlist").addEventListener("click", event => {
     /*
-        Collect the user put by selecting the input fields, one
-        at a time, and accessing the `value` property
+        "saveWishlist" is the button you are creating so the id of the button needs to match
+        with the above id and 
+        you want it to listen to the event of being clicked on
     */
+
+   // these are the fields you are populating below//
     const thing = document.querySelector("#wishListItem").value
     const place = document.querySelector("#wishListLocation").value
 
-    // Once you have collected all the values, update the DOM
-    // with some HTML
+    /* Once you have collected all the values, update the DOM with some HTML
+    make sure your HTML matches 
+    */
     container.innerHTML += `
-        <section>
-            <h1>I can buy ${thing} at ${place}</h1>
-        </section>
+    <section>  
+    <div id="container"></div>      
+    <h1> I can purchase ${thing} at ${place}</h1>
+    </section>
     `
 
 })
